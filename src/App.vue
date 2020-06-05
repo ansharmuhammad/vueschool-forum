a7fe2512ab4f36814d4b9f54dd2088326fae846e
anshar <muhammad.muhammad.anshar@gmail.com>
2020-06-06 05:53:27 +0700

    lesson 9
    intro to vue router

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,9 @@
 <template>
   <div id="app">
     <img src="./assets/logo.png">
-    <dir class="container">
+    <div class="container">
       <router-view/>
-    </dir>
+    </div>
   </div>
 </template>
 
