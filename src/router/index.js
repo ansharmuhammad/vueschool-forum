fe5e6963243c94ca585a42d2cd690a1f0db7cbac
anshar <muhammad.muhammad.anshar@gmail.com>
2020-06-06 19:48:13 +0700

    action and mutation for thread create page

--- src/router/index.js
+++ src/router/index.js
@@ -1,55 +1,95 @@
 import Vue from 'vue'
-import Router from 'vue-router'
-import Home from '@/pages/PageHome'
-import ThreadShow from '@/pages/PageThreadShow'
-import Category from '@/pages/PageCategory'
-import Forum from '@/pages/PageForum'
-import Profile from '@/pages/PageProfile'
-import NotFound from '@/pages/PageNotFound'
-Vue.use(Router)
+import Vuex from 'vuex'
+import sourceData from '@/data'
 
-export default new Router({
-  routes: [
-    {
-      path: '/',
-      name: 'Home',
-      component: Home
-    },
-    {
-      path: '/category/:id',
-      name: 'Category',
-      component: Category,
-      props: true
-    },
-    {
-      path: '/forum/:id',
-      name: 'Forum',
-      component: Forum,
-      props: true
-    },
-    {
-      path: '/thread/:id',
-      name: 'ThreadShow',
-      component: ThreadShow,
-      props: true
-    },
-    {
-      path: '/me',
-      name: 'Profile',
-      component: Profile,
-      props: true
-    },
-    {
-      path: '/me/edit',
-      name: 'ProfileEdit',
-      component: Profile,
-      props: {edit: true}
-    },
-    {
-      path: '*',
-      name: 'NotFound',
-      component: NotFound
+Vue.use(Vuex)
+
+export default new Vuex.Store({
+  state: {
+    ...sourceData,
+    authId: 'VXjpr2WHa8Ux4Bnggym8QFLdv5C3'
+  },
+
+  getters: {
+    authUser (state) {
+      return state.users[state.authId]
     }
-  ],
-  mode: 'history'
+  },
+
+  actions: {
+    createPost ({commit, state}, post) {
+      const postId = 'greatPost' + Math.random()
+      post['.key'] = postId
+      post.userId = state.authId
+      post.publishedAt = Math.floor(Date.now() / 1000)
+
+      commit('setPost', {post, postId})
+      commit('appendPostToThread', {threadId: post.threadId, postId})
+      commit('appendPostToUser', {userId: post.userId, postId})
+    },
+
+    createThread ({state, commit, dispatch}, {text, title, forumId}) {
+      const threadId = 'greatThread' + Math.random()
+      const userId = state.authId
+      const publishedAt = Math.floor(Date.now() / 1000)
+
+      const thread = {'.key': threadId, title, forumId, publishedAt, userId}
+
+      commit('setThread', {threadId, thread})
+      commit('appendThreadToForum', {forumId, threadId})
+      commit('appendThreadToUser', {userId, threadId})
+
+      dispatch('createPost', {text, threadId})
+    },
+
+    updateUser ({commit}, user) {
+      commit('setUser', {userId: user['.key'], user})
+    }
+  },
+
+  mutations: {
+    setPost (state, {post, postId}) {
+      Vue.set(state.posts, postId, post)
+    },
+
+    setUser (state, {user, userId}) {
+      Vue.set(state.users, userId, user)
+    },
+
+    setThread (state, {thread, threadId}) {
+      Vue.set(state.threads, threadId, thread)
+    },
+
+    appendPostToThread (state, {postId, threadId}) {
+      const thread = state.threads[threadId]
+      if (!thread.posts) {
+        Vue.set(thread, 'posts', {})
+      }
+      Vue.set(thread.posts, postId, postId)
+    },
+
+    appendPostToUser (state, {postId, userId}) {
+      const user = state.users[userId]
+      if (!user.posts) {
+        Vue.set(user, 'posts', {})
+      }
+      Vue.set(user.posts, postId, postId)
+    },
+
+    appendThreadToForum (state, {forumId, threadId}) {
+      const forum = state.forums[forumId]
+      if (!forum.threads) {
+        Vue.set(forum, 'threads', {})
+      }
+      Vue.set(forum.threads, threadId, threadId)
+    },
+
+    appendThreadToUser (state, {userId, threadId}) {
+      const user = state.users[userId]
+      if (!user.threads) {
+        Vue.set(user, 'threads', {})
+      }
+      Vue.set(user.threads, threadId, threadId)
+    }
+  }
 })
