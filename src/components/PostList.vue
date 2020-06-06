ac618acf5eeb0bb1c0f6513c67a941c68bff38d9
anshar <muhammad.muhammad.anshar@gmail.com>
2020-06-06 20:26:35 +0700

    add feature: update post

--- src/components/PostList.vue
+++ src/components/PostList.vue
@@ -1,23 +1,57 @@
 <template>
-  <div class="post-list">
-    <PostListItem
-      v-for="post in posts"
-      :post="post"
-      :key="post['.key']"
-    />
+  <div class="post">
+    <div class="user-info">
+      <a href="#" class="user-name">{{user.name}}</a>
+
+      <a href="#">
+        <img class="avatar-large" :src="user.avatar" alt="">
+      </a>
+
+      <p class="desktop-only text-small">{{userPostsCount}} posts</p>
+    </div>
+
+    <div class="post-content">
+      <div v-if="!editing">
+        {{post.text}}
+      </div>
+      <div v-else>
+        <PostEditor
+          :post="post"
+          @save="editing = false"
+        />
+      </div>
+    </div>
+
+    <div class="post-date text-faded">
+      <AppDate :timestamp="post.publishedAt"/>
+    </div>
   </div>
 </template>
 
 <script>
-    import PostListItem from './PostListItem'
+    import {countObjectProperties} from '@/utils'
+    import PostEditor from './PostEditor'
     export default {
-      components: {
-        PostListItem
-      },
       props: {
-        posts: {
+        post: {
           required: true,
-          type: Array
+          type: Object
+        }
+      },
+      components: {
+        PostEditor
+      },
+      data () {
+        return {
+          editing: false
+        }
+      },
+      computed: {
+        user () {
+          return this.$store.state.users[this.post.userId]
+        },
+        userPostsCount () {
+          return countObjectProperties(this.user.posts)
         }
       }
     }
