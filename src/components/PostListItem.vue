5b1188fe0d9e4fa5c565828437ddb5a4f9d99a3e
anshar <muhammad.muhammad.anshar@gmail.com>
2020-06-06 17:28:06 +0700

    migrating to vuex

--- src/components/PostListItem.vue
+++ src/components/PostListItem.vue
@@ -1,42 +1,32 @@
 <template>
-  <div class="post">
-    <div class="user-info">
-      <a href="#" class="user-name">{{user.name}}</a>
+  <div class="forum-list">
 
-      <a href="#">
-        <img class="avatar-large" :src="user.avatar" alt="">
-      </a>
+    <h2 class="list-title">
+      <router-link :to="{name: 'Category', params: {id: category['.key']}}">
+        {{ category.name }}
+      </router-link>
+    </h2>
 
-      <p class="desktop-only text-small">{{userPostsCount}} posts</p>
-    </div>
-
-    <div class="post-content">
-      <div>
-        {{post.text}}
-      </div>
-    </div>
-
-    <div class="post-date text-faded">
-      <AppDate :timestamp="post.publishedAt"/>
-    </div>
+    <ForumList :forums="categoryForums"/>
   </div>
 </template>
 
 <script>
-    import sourceData from '@/data'
+    import ForumList from './ForumList'
     export default {
+      components: {
+        ForumList
+      },
       props: {
-        post: {
+        category: {
           required: true,
           type: Object
         }
       },
       computed: {
-        user () {
-          return sourceData.users[this.post.userId]
-        },
-        userPostsCount () {
-          return Object.keys(this.user.posts).length
+        categoryForums () {
+          return Object.values(this.$store.state.forums)
+            .filter(forum => forum.categoryId === this.category['.key'])
         }
       }
     }
--- src/pages/PageThreadShow.vue
+++ src/pages/PageThreadShow.vue
@@ -14,7 +14,6 @@
 </template>
 
 <script>
-  import sourceData from '@/data'
   import PostList from '@/components/PostList'
   import PostEditor from '@/components/PostEditor'
   export default {
@@ -30,23 +29,23 @@
     },
     data () {
       return {
-        thread: sourceData.threads[this.id],
+        thread: this.$store.state.threads[this.id],
         newPostText: ''
       }
     },
     computed: {
       posts () {
         const postIds = Object.values(this.thread.posts)
-        return Object.values(sourceData.posts)
+        return Object.values(this.$store.state.posts)
           .filter(post => postIds.includes(post['.key']))
       }
     },
     methods: {
       addPost ({post}) {
         const postId = post['.key']
-        this.$set(sourceData.posts, postId, post)
+        this.$set(this.$store.state.posts, postId, post)
         this.$set(this.thread.posts, postId, postId)
-        this.$set(sourceData.users[post.userId].posts, postId, postId)
+        this.$set(this.$store.state.users[post.userId].posts, postId, postId)
       }
     }
   }
