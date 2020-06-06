e4a3376e0906a756b37e8108060d622ceb217bdf
anshar <muhammad.muhammad.anshar@gmail.com>
2020-06-07 00:00:30 +0700

    auth user profile page

--- src/store/getters.js
+++ src/store/getters.js
@@ -1,8 +1,18 @@
+  
 import {countObjectProperties} from '@/utils'
 
 export default {
   authUser (state) {
     return state.authId ? state.users[state.authId] : null
+  },
+
+  userPosts: state => id => {
+    const user = state.users[id]
+    if (user.posts) {
+      return Object.values(state.posts)
+        .filter(post => post.userId === id)
+    }
+    return []
   },
 
   userThreadsCount: state => id => countObjectProperties(state.users[id].threads),
