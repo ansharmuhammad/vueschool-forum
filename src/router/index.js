5677820135b16faddeb9cbcbc291775e150ad17d
anshar <muhammad.muhammad.anshar@gmail.com>
2020-06-06 23:42:29 +0700

    sign users in and out

--- src/router/index.js
+++ src/router/index.js
@@ -1,21 +1,81 @@
 import Vue from 'vue'
-import Vuex from 'vuex'
-import actions from './actions'
-import mutations from './mutations'
-import getters from './getters'
+import Router from 'vue-router'
+import Home from '@/pages/PageHome'
+import ThreadShow from '@/pages/PageThreadShow'
+import ThreadCreate from '@/pages/PageThreadCreate'
+import ThreadEdit from '@/pages/PageThreadEdit'
+import Category from '@/pages/PageCategory'
+import Forum from '@/pages/PageForum'
+import Profile from '@/pages/PageProfile'
+import Register from '@/pages/PageRegister'
+import SignIn from '@/pages/PageSignIn'
+import NotFound from '@/pages/PageNotFound'
+Vue.use(Router)
 
-Vue.use(Vuex)
-
-export default new Vuex.Store({
-  state: {
-    categories: {},
-    forums: {},
-    threads: {},
-    posts: {},
-    users: {},
-    authId: 'VXjpr2WHa8Ux4Bnggym8QFLdv5C3'
-  },
-  getters,
-  actions,
-  mutations
+export default new Router({
+  routes: [
+    {
+      path: '/',
+      name: 'Home',
+      component: Home
+    },
+    {
+      path: '/category/:id',
+      name: 'Category',
+      component: Category,
+      props: true
+    },
+    {
+      path: '/forum/:id',
+      name: 'Forum',
+      component: Forum,
+      props: true
+    },
+    {
+      path: '/thread/create/:forumId',
+      name: 'ThreadCreate',
+      component: ThreadCreate,
+      props: true
+    },
+    {
+      path: '/thread/:id',
+      name: 'ThreadShow',
+      component: ThreadShow,
+      props: true
+    },
+    {
+      path: '/thread/:id/edit',
+      name: 'ThreadEdit',
+      component: ThreadEdit,
+      props: true
+    },
+    {
+      path: '/me',
+      name: 'Profile',
+      component: Profile,
+      props: true
+    },
+    {
+      path: '/me/edit',
+      name: 'ProfileEdit',
+      component: Profile,
+      props: {edit: true}
+    },
+    {
+      path: '/register',
+      name: 'Register',
+      component: Register
+    },
+    {
+      path: '/signin',
+      name: 'SignIn',
+      component: SignIn
+    },
+    {
+      path: '*',
+      name: 'NotFound',
+      component: NotFound
+    }
+  ],
+  mode: 'history'
 })
--- src/store/actions.js
+++ src/store/actions.js
@@ -55,6 +55,38 @@ export default {
     })
   },
 
+  createUser ({state, commit}, {id, email, name, username, avatar = null}) {
+    return new Promise((resolve, reject) => {
+      const registeredAt = Math.floor(Date.now() / 1000)
+      const usernameLower = username.toLowerCase()
+      email = email.toLowerCase()
+      const user = {avatar, email, name, username, usernameLower, registeredAt}
+      firebase.database().ref('users').child(id).set(user)
+        .then(() => {
+          commit('setItem', {resource: 'users', id: id, item: user})
+          resolve(state.users[id])
+        })
+    })
+  },
+
+  registerUserWithEmailAndPassword ({dispatch}, {email, name, username, password, avatar = null}) {
+    return firebase.auth().createUserWithEmailAndPassword(email, password)
+      .then(user => {
+        return dispatch('createUser', {id: user.uid, email, name, username, password, avatar})
+      })
+  },
+
+  signInWithEmailAndPassword (context, {email, password}) {
+    return firebase.auth().signInWithEmailAndPassword(email, password)
+  },
+
+  signOut ({commit}) {
+    return firebase.auth().signOut()
+      .then(() => {
+        commit('setAuthId', null)
+      })
+  },
+
   updateThread ({state, commit, dispatch}, {title, text, id}) {
     return new Promise((resolve, reject) => {
       const thread = state.threads[id]
@@ -100,6 +132,13 @@ export default {
     commit('setUser', {userId: user['.key'], user})
   },
 
+  fetchAuthUser ({dispatch, commit}) {
+    const userId = firebase.auth().currentUser.uid
+    return dispatch('fetchUser', {id: userId})
+      .then(() => {
+        commit('setAuthId', userId)
+      })
+  },
   fetchCategory: ({dispatch}, {id}) => dispatch('fetchItem', {resource: 'categories', id, emoji: '🏷'}),
   fetchForum: ({dispatch}, {id}) => dispatch('fetchItem', {resource: 'forums', id, emoji: '🌧'}),
   fetchThread: ({dispatch}, {id}) => dispatch('fetchItem', {resource: 'threads', id, emoji: '📄'}),
