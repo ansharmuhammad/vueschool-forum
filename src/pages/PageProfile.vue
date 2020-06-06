dab02c8284682999e53e7423f4eac38c3522292b
anshar <muhammad.muhammad.anshar@gmail.com>
2020-06-06 23:51:57 +0700

    create logout route using per route guards

--- src/pages/PageProfile.vue
+++ src/pages/PageProfile.vue
@@ -30,7 +30,6 @@
     import UserProfileCard from '@/components/UserProfileCard'
     import UserProfileCardEditor from '@/components/UserProfileCardEditor'
     import {mapGetters} from 'vuex'
-    import store from '@/store'
     export default {
       components: {
         PostList,
@@ -53,13 +52,6 @@
               .filter(post => post.userId === this.user['.key'])
           }
           return []
-        }
-      },
-      beforeRouteEnter (to, from, next) {
-        if (store.state.authId) {
-          next()
-        } else {
-          next({name: 'Home'})
         }
       },
       created () {
