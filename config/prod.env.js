f18262912697868ac8049437d9465342f37214e9
anshar <muhammad.muhammad.anshar@gmail.com>
2020-06-06 23:22:36 +0700

    install dotenv

--- config/prod.env.js
+++ config/prod.env.js
@@ -1,12 +1,12 @@
 'use strict'
 module.exports = {
-  FIREBASE_API_KEY: 'AIzaSyB_lwl3M4BKhWycMbCxupxV13FQIGC4BXM',
-  FIREBASE_AUTH_DOMAIN: 'vue-school-forum-f17d2.firebaseapp.com',
-  FIREBASE_DATABASE_URL: 'https://vue-school-forum-f17d2.firebaseio.com',
-  FIREBASE_PROJECT_ID: 'vue-school-forum-f17d2',
-  FIREBASE_STORAGE_BUCKET: 'vue-school-forum-f17d2.appspot.com',
-  FIREBASE_MESSAGING_ID: '507714797983',
-  FIREBASE_APP_ID: '1:507714797983:web:02dd9ca0d9ac15d9293daa',
-  FIREBASE_MEASUREMENT_ID: 'G-611E461HV9',
-  FIREBASE_NODE_ENV: '"production"'
+  NODE_ENV: '"production"',
+  FIREBASE_API_KEY: JSON.stringify(process.env.FIREBASE_API_KEY),
+  FIREBASE_AUTH_DOMAIN: JSON.stringify(process.env.FIREBASE_AUTH_DOMAIN),
+  FIREBASE_DATABASE_URL: JSON.stringify(process.env.FIREBASE_DATABASE_URL),
+  FIREBASE_PROJECT_ID: JSON.stringify(process.env.FIREBASE_PROJECT_ID),
+  FIREBASE_STORAGE_BUCKET: JSON.stringify(process.env.FIREBASE_STORAGE_BUCKET),
+  FIREBASE_MESSAGING_ID: JSON.stringify(process.env.FIREBASE_MESSAGING_ID)
+  FIREBASE_APP_ID: JSON.stringify(process.env.FIREBASE_APP_ID)
+  FIREBASE_MEASUREMENT_ID: JSON.stringify(process.env.FIREBASE_MEASUREMENT_ID)
 }
